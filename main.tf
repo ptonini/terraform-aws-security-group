8995d00f74c87457bfd621c1da8b52e7971b97b7
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-12-03 23:56:30 -0300

    chore: updated dependency sources

--- main.tf
+++ main.tf
@@ -1,8 +1,23 @@
+locals {
+  builtin_rules = {
+    all = {from_port = 0, protocol = -1}
+    http = {from_port = 80}
+    https = {from_port = 443}
+    ping = {from_port = 8, to_port = 0, protocol = "icmp"}
+    ssh = {from_port = 22}
+    nodeports_tcp = {protocol = "tcp", from_port = 30000, to_port = 32767}
+    nodeports_udp = {protocol = "udp", from_port = 30000, to_port = 32767}
+    mongodb = {from_port = 27017}
+    postgres = {from_port = 5432}
+    mysql = {from_port = 3306}
+  }
+}
+
 resource "aws_security_group" "this" {
   vpc_id = var.vpc.id
   description = null
   dynamic "ingress" {
-    for_each = local.ingress_rules
+    for_each = merge(var.ingress_rules, {for r in var.builtin_ingress_rules : r => local.builtin_rules[r]})
     content {
       from_port = ingress.value["from_port"]
       to_port = try(ingress.value["to_port"], ingress.value["from_port"])
@@ -14,7 +29,7 @@ resource "aws_security_group" "this" {
     }
   }
   dynamic "egress" {
-    for_each = local.egress_rules
+    for_each = merge(var.egress_rules, {for r in var.builtin_egress_rules : r => local.builtin_rules[r]})
     content {
       from_port = egress.value["from_port"]
       to_port = try(egress.value["to_port"], egress.value["from_port"])

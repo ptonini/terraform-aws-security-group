d892c159c361f5cf0bb0242d57c6f4c5305496b0
ptonini <pedro.tonini@hotmail.com>
2023-10-06 16:09:57 -0300

    fix: removed default value for cidr_blocks

--- variables.tf
+++ variables.tf
@@ -9,7 +9,7 @@ variable "ingress_rules" {
     from_port        = number
     to_port          = optional(number)
     protocol         = optional(string, "tcp")
-    cidr_blocks      = optional(set(string), ["0.0.0.0/0"])
+    cidr_blocks      = optional(set(string))
     ipv6_cidr_blocks = optional(set(string))
     prefix_list_ids  = optional(set(string))
     security_groups  = optional(set(string))
@@ -23,7 +23,7 @@ variable "egress_rules" {
     from_port        = number
     to_port          = optional(number)
     protocol         = optional(string, "tcp")
-    cidr_blocks      = optional(set(string), ["0.0.0.0/0"])
+    cidr_blocks      = optional(set(string))
     ipv6_cidr_blocks = optional(set(string))
     prefix_list_ids  = optional(set(string))
     security_groups  = optional(set(string))
